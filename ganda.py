6cd1366b2037fee2906ad2c0722b75c50a60bcf2
Azmi Nailal Hadi <128576939+MoccaGoddd@users.noreply.github.com>
2023-04-11 14:10:22 +0700

    Add files via upload

---
+++ ganda.py
@@ -0,0 +1,4 @@
+nilai = input("Masukan nilai :")
+nilai_ganda = int(nilai) * 2
+print(nilai)
+print("Nilai jika digandakan " + str(nilai_ganda))
---
+++ meter.py
@@ -0,0 +1,7 @@
+meter = input("Masukan nilai meter :")
+
+cm = int(meter) * 100
+inc = int(meter) * 100 /2.54
+
+print("Nilai dalam cm : " + str(cm))
+print("Nilai dalam inc : " + str(inc))
